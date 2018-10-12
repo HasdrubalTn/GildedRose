0af80e0903f288de2a75c56b56ae89fb7200e171
Ali Mselmi <mselmi@outlook.com>
2018-10-13 00:50:10 +0200

    extract methods for each rose type

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -38,70 +38,90 @@ namespace GildedRose.Console
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name == "Aged Brie")
+                switch (Items[i].Name)
                 {
-                    if (Items[i].Quality < 50)
+                    case "Aged Brie":
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].SellIn < 0)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
+                        UpdateAgedBrieQuality(i);
+                        break;
                     }
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality < 50)
+                    case "Backstage passes to a TAFKAL80ETC concert":
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 0)
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
+                        UpdateBackStageQuality(i);
+                        break;
                     }
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                else
-                {
-                    Items[i].Quality = Items[i].Quality - 1;
-
-                    if (Items[i].SellIn < 0)
+                    default:
                     {
-                        if (Items[i].Quality > 0)
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
+                        UpdateOrdinaryQuality(i);
+                        break;
                     }
-
-                    Items[i].SellIn = Items[i].SellIn - 1;
                 }
             }
         }
 
+        private void UpdateOrdinaryQuality(int i)
+        {
+            Items[i].Quality = Items[i].Quality - 1;
+
+            if (Items[i].SellIn < 0)
+            {
+                if (Items[i].Quality > 0)
+                {
+                    Items[i].Quality = Items[i].Quality - 1;
+                }
+            }
+
+            Items[i].SellIn = Items[i].SellIn - 1;
+        }
+
+        private void UpdateBackStageQuality(int i)
+        {
+            if (Items[i].Quality < 50)
+            {
+                Items[i].Quality = Items[i].Quality + 1;
+
+                if (Items[i].SellIn < 11)
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+                    }
+                }
+
+                if (Items[i].SellIn < 6)
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+                    }
+                }
+
+                if (Items[i].SellIn < 0)
+                {
+                    Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                }
+            }
+
+            Items[i].SellIn = Items[i].SellIn - 1;
+        }
+
+        private void UpdateAgedBrieQuality(int i)
+        {
+            if (Items[i].Quality < 50)
+            {
+                Items[i].Quality = Items[i].Quality + 1;
+
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+                    }
+                }
+            }
+
+            Items[i].SellIn = Items[i].SellIn - 1;
+        }
     }
 
     public class Item
