321192fbf5ed264425c55d60a4d2cc627c06368a
Ali Mselmi <mselmi@outlook.com>
2018-10-13 00:45:03 +0200

    refactoring UpdateQuality preparing for Strategy pattern

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -38,74 +38,66 @@ namespace GildedRose.Console
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
+                if (Items[i].Name == "Aged Brie")
                 {
                     if (Items[i].Quality < 50)
                     {
                         Items[i].Quality = Items[i].Quality + 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].SellIn < 0)
                         {
-                            if (Items[i].SellIn < 11)
+                            if (Items[i].Quality < 50)
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
+                                Items[i].Quality = Items[i].Quality + 1;
                             }
                         }
                     }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
-                if (Items[i].SellIn < 0)
+                else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Quality < 50)
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        Items[i].Quality = Items[i].Quality + 1;
+
+                        if (Items[i].SellIn < 11)
                         {
-                            if (Items[i].Quality > 0)
+                            if (Items[i].Quality < 50)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
+                                Items[i].Quality = Items[i].Quality + 1;
                             }
                         }
-                        else
+
+                        if (Items[i].SellIn < 6)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
+
+                        if (Items[i].SellIn < 0)
                         {
                             Items[i].Quality = Items[i].Quality - Items[i].Quality;
                         }
                     }
-                    else
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                }
+
+                else
+                {
+                    Items[i].Quality = Items[i].Quality - 1;
+
+                    if (Items[i].SellIn < 0)
                     {
-                        if (Items[i].Quality < 50)
+                        if (Items[i].Quality > 0)
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            Items[i].Quality = Items[i].Quality - 1;
                         }
                     }
+
+                    Items[i].SellIn = Items[i].SellIn - 1;
                 }
             }
         }
